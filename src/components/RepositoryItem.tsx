2f82168228d03b2380c00aeb1a64f9b7dabb761b
viniciusoliveiras <vinitag190@gmail.com>
2021-04-16 18:06:35 -0300

    Busca de repos de usuario digitado

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -1,18 +1,35 @@
+import { MdKeyboardArrowRight } from 'react-icons/md';
+
+import userImg from '../assets/github.png';
+
 interface RepositoryItemProps {
   repository: {
     name: string;
     description: string;
     html_url: string;
   };
+  avatar_url: string;
+  login: string;
 }
 
-export function RepositoryItem(props: RepositoryItemProps) {
+export function RepositoryItem({
+  repository,
+  avatar_url,
+  login,
+}: RepositoryItemProps) {
+  const avatar = avatar_url;
   return (
-    <div className="repository-item">
-      <a href={props.repository.html_url}>
-        <strong>{props.repository.name}</strong>
-        <p>{props.repository.description}</p>
-      </a>
-    </div>
+    <a href={repository.html_url} className='repository-item' target='blank'>
+      <div>
+        <img src={avatar || userImg} alt='github' />
+        <div>
+          <strong>
+            {login}/{repository.name}
+          </strong>
+          <p>{repository.description}</p>
+        </div>
+      </div>
+      <MdKeyboardArrowRight fontSize='45px' color='#e1e1e8' />
+    </a>
   );
 }
--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,9 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, FormEvent } from 'react';
 import { RepositoryItem } from './RepositoryItem';
 
 import '../styles/repositories.scss';
 
 import logoImg from '../assets/logo.svg';
+import { api } from '../services/api';
 
 interface Repository {
   name: string;
@@ -11,8 +12,35 @@ interface Repository {
   html_url: string;
 }
 
+interface UserInfos {
+  avatar_url: string;
+}
+
 export function RepositoryList() {
   const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [username, setUsername] = useState('');
+  const [userAvatar, setUserAvatar] = useState(
+    'https://avatars.githubusercontent.com/u/64497059?v=4'
+  );
+  const [userName, setUserName] = useState('viniciusoliveiras');
+
+  function handleSearchUserRepos(event: FormEvent) {
+    event.preventDefault();
+    searchUser();
+    setUsername('');
+  }
+
+  function searchUser() {
+    fetch(`https://api.github.com/users/${username}/repos`).then((response) =>
+      response.json().then((data) => setRepositories(data))
+    );
+
+    api
+      .get(`${username}`)
+      .then((response) => setUserAvatar(response.data.avatar_url));
+
+    api.get(`${username}`).then((response) => setUserName(response.data.login));
+  }
 
   useEffect(() => {
     fetch(
@@ -28,18 +56,28 @@ export function RepositoryList() {
         Explore repositórios <br /> no Github.
       </h1>
 
-      <div className='searchRepo'>
+      <form className='searchRepo' onSubmit={handleSearchUserRepos}>
         <input
           type='text'
-          name='searchRepo'
-          id='searchRepo'
-          placeholder='Digite aqui'
+          name='username'
+          id='username'
+          placeholder='Digite aqui o nome do usuário'
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+          required
         />
-        <button type='button'>Pesquisar</button>
-      </div>
+        <button type='submit'>Pesquisar</button>
+      </form>
 
       {repositories.map((repository) => {
-        return <RepositoryItem key={repository.name} repository={repository} />;
+        return (
+          <RepositoryItem
+            key={repository.name}
+            repository={repository}
+            avatar_url={userAvatar}
+            login={userName}
+          />
+        );
       })}
     </section>
   );
